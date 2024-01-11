67c56cf5c1e04ea74dc638866069948735c45bf9
hicham <hicham@DESKTOP-SDCLOI7>
2024-01-11 21:04:14 +0100

    area cilindro

--- sistemasDeControl/src/sistemasDeControl/calcular_areas.java
+++ sistemasDeControl/src/sistemasDeControl/calcular_areas.java
@@ -16,10 +16,33 @@ public class calcular_areas {
 
 	        System.out.println("El área del triángulo es: " + area);
 
+	     
+	       
+	        System.out.println("Ingrese el radio de la base del cilindro:");
+	        double radio = scanner.nextDouble();
+
+	        System.out.println("Ingrese la altura del cilindro:");
+	        double altura = scanner.nextDouble();
+
+	       
+	        double area2 = calcularAreaCilindro(radio, altura);
+
+	       
+	        System.out.println("El área del cilindro es: " + area2);
+	        
 	        
 	    }
 
 	    public static double calcularArea(int lado1, int lado2) {
 	        return (double) (lado1 * lado2) / 2;
+	    }
+	    public static double calcularAreaCilindro(double radio, double altura) {
+	       
+	        double areaBases = 2 * Math.PI * Math.pow(radio, 2);
+	     
+	        double areaLateral = 2 * Math.PI * radio * altura;
+	        
+	       
+	        return areaBases + areaLateral;
 	    }
 }
